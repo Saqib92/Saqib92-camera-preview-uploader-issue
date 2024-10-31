461c5b66afc9a47ceca3ef192b1d37c8f2297676
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2024-10-31 15:32:39 +0500

    example setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,99 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonButton } from '@ionic/angular/standalone';
+import { CameraPreview, CameraPreviewOptions } from '@capgo/camera-preview'
+import { Uploader } from '@capgo/capacitor-uploader';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonButton],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  isRecording: boolean = false;
+  isCameraOpen: boolean = false;
+  constructor() { }
+
+  ionViewWillEnter() {
+
+  }
+
+  async startCamera() {
+    this.isCameraOpen = true;
+    let opt: CameraPreviewOptions = {
+      position: 'rear',
+      toBack: true
+    }
+
+    await CameraPreview.start(opt);
+  }
+
+  ionViewWillLeave() {
+    setTimeout(async () => {
+      await CameraPreview.stop();
+    }, 1000)
+  }
+
+  ngOnInit() {
+  }
+
+
+  async flip() {
+    await CameraPreview.flip();
+  }
+
+  async startRecording() {
+    this.isRecording = true;
+    await CameraPreview.startRecordVideo({ storeToFile: true })
+  }
+
+  async stopRecording() {
+    this.isRecording = false;
+    const fileUrl = await CameraPreview.stopRecordVideo()
+    console.log(fileUrl)
+    setTimeout(async () => {
+      await this.uploadVideo(fileUrl.videoFilePath)
+    }, 1000)
+
+  }
+
+  async uploadVideo(filePath: string) {
+    // events not working
+    Uploader.addListener('events', (event) => {
+      console.log(event);
+      switch (event.name) {
+        case 'uploading':
+          console.log(`Upload progress: ${event.payload.percent}%`);
+          break;
+        case 'completed':
+          console.log('Upload completed successfully');
+          console.log('Server response status code:', event.payload.statusCode);
+          break;
+        case 'failed':
+          console.error('Upload failed:', event.payload.error);
+          break;
+      }
+    });
+
+    try {
+      const result = await Uploader.startUpload({
+        filePath,
+        serverUrl: '', // add server url
+        method: 'POST',
+        headers: {
+          'Content-Type': 'video/mp4',
+          'Authorization': 'Bearer ' + 'My token here'
+        },
+        mimeType: 'video/mp4',
+      });
+      //always cnsole video uploaded. and notification shows upload error
+      console.log('Video uploaded successfully:', result.id);
+
+    } catch (error) {
+      console.error('Error uploading video:', error);
+      throw error;
+    }
+  }
 }
