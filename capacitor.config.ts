9a688732ee372c1fa1ba8b951d944e58ea230473
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2024-11-06 18:14:24 +0500

    updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wizdiary.app',
   appName: 'campeview-uploader',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    GoogleAuth: {
+      scopes: ["profile", "email"],
+      serverClientId: "1077151725207-ji53c56mjpo4pet9kacqknkboc8t2g4i.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true
+    }
+  }
 };
 
 export default config;
